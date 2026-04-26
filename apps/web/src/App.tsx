e03d67f52b6b665670483e54e8e85f4cbb559934
Guilherme Ferreira Lenzolari <guilhermeferreiralenzolari@MacBook-Pro-de-Guilherme-2.local>
2026-04-25 22:25:57 -0300

    feat(FE): create buttons layout: button container, image and name

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -27,17 +27,26 @@ function App() {
 
   const columns = isPortrait ? 1 : 3;
 
-  const gridWidth = isPortrait ? "50vw" : "70vw";
-  const gridHeight = isPortrait ? "50vh" : "30vh";
+  const gridWidth = isPortrait ? 50 : 70;
+  const gridHeight = isPortrait ? 40 : 25;
+
+  const gap = 2;
+
+  const buttonHeight = isPortrait
+    ? (gridHeight - gap * (label.length - 1)) / label.length
+    : gridHeight;
+  const buttonWidth = isPortrait
+    ? gridWidth
+    : (gridWidth - gap * (label.length - 1)) / label.length;
 
   const ButtonsList = (
     <div
       style={{
         display: "grid",
         gridTemplateColumns: `repeat(${columns}, 1fr)`,
-        gap: "2vw",
-        width: gridWidth,
-        height: gridHeight,
+        gap: isPortrait ? `${gap}vh` : `${gap}vw`,
+        width: `${gridWidth}vw`,
+        height: `${gridHeight}vh`,
       }}
     >
       {label.map((candidate) => (
@@ -45,6 +54,10 @@ function App() {
           key={candidate}
           label={candidate}
           onVote={() => alert(`You voted for ${candidate}!`)}
+          isPortrait={isPortrait}
+          height={buttonHeight}
+          width={buttonWidth}
+          imageUrl={`https://images.unsplash.com/photo-1770010314670-464a6d221858?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`}
         />
       ))}
     </div>
@@ -52,7 +65,7 @@ function App() {
 
   const titleText = (
     <div>
-      <h1 style={{ lineHeight: "1.2", fontSize: "32px", margin: "0 0 8px" }}>
+      <h1 style={{ lineHeight: "1.2", fontSize: "32px", margin: "0 10px 8px" }}>
         {homepage.title}
       </h1>
       <h3>{homepage.description}</h3>
--- apps/web/src/components/VoteButton.tsx
+++ apps/web/src/components/VoteButton.tsx
@@ -3,30 +3,93 @@ interface VoteButtonProps {
   onVote: () => void;
   height?: number;
   width?: number;
+  isPortrait?: boolean;
+  imageUrl?: string;
 }
 
-const ButtonContent = ({ label }: { label: string }) => {
+const getImageDimensions = (
+  isPortrait?: boolean,
+  width?: number,
+  height?: number,
+) => {
+  const commonStyles = {
+    flexShrink: 0,
+    borderRadius: "5px",
+    margin: 1,
+    objectFit: "cover" as const,
+  };
+  if (isPortrait) {
+    return {
+      width: `${height}vh`,
+      height: `${height}vh`,
+      ...commonStyles,
+    };
+  }
+
+  return {
+    width: `${width}vw`,
+    height: `${height * 0.7}vh`,
+    ...commonStyles,
+  };
+};
+
+const text = (label: string) => {
   return (
-    <div>
-      <span>{label}</span>
+    <div
+      style={{
+        flex: 1,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <h2>{label}</h2>
     </div>
   );
 };
 
-export const VoteButton = ({ label, onVote }: VoteButtonProps) => {
+export const VoteButton = ({
+  label,
+  onVote,
+  isPortrait,
+  imageUrl,
+  width,
+  height,
+}: VoteButtonProps) => {
   return (
     <button
       className="counter"
       onClick={onVote}
       style={{
         cursor: "pointer",
-        opacity: 1,
-        width: "100%",
-        height: "100%",
         margin: 0,
       }}
     >
-      {ButtonContent({ label })}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: isPortrait ? "row" : "column",
+          alignItems: "center",
+          width: "100%",
+          height: "100%",
+          overflow: "hidden",
+          flexShrink: 0,
+          borderRadius: "5px",
+        }}
+      >
+        {isPortrait && text(label)}
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt={label}
+            style={getImageDimensions(isPortrait, width, height)}
+          />
+        )}
+        {!isPortrait && text(label)}
+      </div>
     </button>
   );
 };
