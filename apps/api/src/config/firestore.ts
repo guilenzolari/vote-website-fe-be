729ef9e56c07716185def507708d5a122f28713e
Guilherme Ferreira Lenzolari <guilhermeferreiralenzolari@MacBook-Pro-de-Guilherme-2.local>
2026-09-24 18:50:49 -0300

    chore(BE): deploy BE on render

--- apps/api/src/config/firestore.ts
+++ apps/api/src/config/firestore.ts
@@ -4,28 +4,37 @@ import { log } from "../utils/logger";
 
 const projectId = process.env.GOOGLE_CLOUD_PROJECT;
 const emulatorHost = process.env.FIRESTORE_EMULATOR_HOST;
+const serviceAccountJson = process.env.FIREBASE_SERVICE_ACCOUNT_JSON; // Nova variável opcional
 
 if (!projectId) {
   throw new Error("GOOGLE_CLOUD_PROJECT is not defined");
 }
 
-export const db = emulatorHost
-  ? new Firestore({
-      projectId,
-      host: emulatorHost,
-      ssl: false,
-      credentials: {
-        client_email: "local-emulator@example.com",
-        private_key: "{}",
-      } as any,
-      customCredentials: {
-        createAuthHeader: () =>
-          Promise.resolve({ Authorization: "Bearer owner" }),
-      } as any,
-    })
-  : new Firestore({
-      projectId,
-    });
+let dbConfig: any = { projectId };
+
+if (emulatorHost) {
+  dbConfig = {
+    projectId,
+    host: emulatorHost,
+    ssl: false,
+    credentials: {
+      client_email: "local-emulator@example.com",
+      private_key: "{}",
+    },
+    customCredentials: {
+      createAuthHeader: () =>
+        Promise.resolve({ Authorization: "Bearer owner" }),
+    },
+  };
+} else if (serviceAccountJson) {
+  // Se estiver em produção e passamos o JSON via variável de ambiente
+  dbConfig = {
+    projectId,
+    credentials: JSON.parse(serviceAccountJson),
+  };
+}
+
+export const db = new Firestore(dbConfig);
 
 log.debug(`[Firestore Setup] Conectando ao projeto: ${projectId}`);
 if (emulatorHost) {
